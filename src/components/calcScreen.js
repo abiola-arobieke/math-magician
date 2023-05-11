2988154b0120baf60cc857dca9b89b18698fa41f
abiola <abiola.arobieke@gmail.com>
2023-05-11 17:31:41 -0400

    Modify calcScreen.js file

--- src/components/calcScreen.js
+++ src/components/calcScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CalcScreen = ({ answer }) => <div className="grid-item display">{answer}</div>;
+const CalcScreen = ({ answer }) => (
+  <div className="grid-item display">
+    {answer.total}
+    {answer.operation}
+    {answer.next}
+  </div>
+);
 
 CalcScreen.propTypes = {
-  answer: PropTypes.string.isRequired,
+  answer: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default CalcScreen;
