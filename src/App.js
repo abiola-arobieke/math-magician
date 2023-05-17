c31d9bcfd2e6328249e4fbc9e39953afb713fc20
abiola <abiola.arobieke@gmail.com>
2023-05-17 16:44:05 -0400

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
-import Homepage from './components/quote';
+import Quote from './components/quote';
+import Navbar from './components/navbar';
+import Homepage from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      <Calculator />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
     </div>
   );
 }
