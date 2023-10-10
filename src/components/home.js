35b1ece3c3488ce41a730dad3f71dafb8995262a
abiola-arobieke <abiola.arobieke@gmail.com>
2023-10-10 23:59:59 +0100

    Update home.js file

--- src/components/home.js
+++ src/components/home.js
@@ -3,25 +3,13 @@ function Homepage() {
     <div className="banner mx-10c">
       <h2>Welcome to our page</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu
-        pulvinar dolor. Integer facilisis erat vel nulla tincidunt, ac ultrices
-        sem interdum. Morbi vehicula finibus feugiat. Integer sit amet mollis
-        ante, ac posuere leo. Nullam nisl mi, luctus suscipit nisi vel, finibus
-        accumsan purus. Mauris varius fringilla sapien. Nulla sodales aliquam
-        justo nec dignissim. Integer molestie aliquam turpis, eu blandit metus
-        imperdiet vel. Praesent tortor quam, pulvinar nec urna vitae, tristique
-        imperdiet est. Morbi ac felis ligula. Pellentesque mattis enim eget mi
-        tempor, sit amet ultrices lorem mattis. Aenean aliquam metus nunc.
-        Vestibulum lacinia lorem a mattis feugiat. Mauris suscipit enim sed odio
-        placerat, id interdum odio commodo. Integer venenatis, magna tempor
-        cursus posuere, dolor nibh vehicula ipsum, ut sagittis urna nulla quis
-        magna. Pellentesque quis eros tincidunt, rutrum eros a, faucibus ante.
-        Cras quis faucibus nunc, quis volutpat purus. Donec sit amet porttitor
-        ante. Pellentesque et lacinia quam. Fusce at interdum nisi. Duis
-        ultrices fringilla lectus, a tincidunt mi rhoncus eu. Aliquam est velit,
-        congue cursus iaculis interdum, rhoncus non lorem. Nam id tortor lacus.
-        Praesent volutpat sagittis leo, a porta mi imperdiet sed. Aenean
-        ultrices venenatis scelerisque. Nulla facilisi.
+        The Math Magician brings convenience and efficiency to the world of
+        calculation. It calculates answers to full accuracy, and is designed to
+        compute very large numbers. The app would be more useful in fields such
+        as cosmology, astronomy, mathematics, cryptography, and statistical
+        mechanics where big number calculations are required. Acceptable formats
+        are integers and decimals. Percentage which represents the ratio of a
+        fraction in 100 can also be calculated. Math Magician is 100% free.
       </p>
     </div>
   );
